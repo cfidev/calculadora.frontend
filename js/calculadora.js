124ab6a3dcc7fc13ea7a317b90b7c09d3f244d7c
aleecmp <ale.cmpb@gmail.com>
2023-04-10 17:03:05 -0400

    fix: add logic for operations

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,137 +1,43 @@
-// $(function() {
-
-//   // let displayVal = '';
-
-//   $('.btn').click(function() {
-//   let buttonVal = $(this).val();
-
-//   switch(buttonVal) {
-//     case 'C':
-//       $('#visor').val('');
-//       break;
-//     case '=':
-//       $('#visor').eval()
-      
-
-    
-//     default:
-
-//   }
-
-//   $('#visor').val($('#visor').val() + buttonVal);
-//   console.log(buttonVal)
-//   })
-  
-  
-// });
-
-//     $().forEach((value) => {
-//     let numbervalue = document.
-//     $(`#button${numbervalue}`).on("click",function(){
-//         $("#visor").append(value);
-//     });
-//   })
-// 
-
-// $(function() {
-//     let result = '';
-//     let operacion = '';
-
-    
-//     $('.btn').click(function() {
-//       let buttonVal = $(this).val();
-//       console.log('soy buttonVal', buttonVal);
-//       console.log(typeof(buttonVal))
-//       console.log(eval('2 * 2')) 
-      
-//       if ($.isNumeric(buttonVal)) {
-//         result += buttonVal;
-//         $('#visor').val(result);
-//       } else {
-//         switch(buttonVal) {
-//           case '+':
-//             operacion = '+';
-//             break;
-//           case '-':
-//             operacion = '-';
-//             break;
-//           case '*':
-//             operacion = '*';
-//             break;
-//           case '/':
-//             operacion = '/';
-//             break;
-//           case '=':
-//             result = eval(result);
-//             $('#visor').val(result);
-//             break;
-//           case 'C':
-//             result = '';
-//             $('#visor').val(result);
-//             break;
-//           // default:
-//           //   return result;
-//         }
-//       }
-//     });
-//   });
-
 $(function() {
-  let result = 0;
+  let result = '';
   let operacion = '';
-  let currentNumber = 0;
 
   $('.btn').click(function() {
     let buttonVal = $(this).val();
-    console.log('soy buttonVal', buttonVal);
-    console.log(typeof(buttonVal))
-    console.log(eval('2 * 2')) 
 
     if ($.isNumeric(buttonVal)) {
-      currentNumber = parseInt(buttonVal);
-      $('#visor').val(currentNumber);
+      result += buttonVal;
+      $('#visor').val(result);
     } else {
       switch(buttonVal) {
         case '+':
-          result += currentNumber;
+          result += ' + ';
           operacion = '+';
           break;
         case '-':
-          result -= currentNumber;
+          result += ' - ';
           operacion = '-';
           break;
         case '*':
-          result *= currentNumber;
+          result += ' * ';
           operacion = '*';
           break;
         case '/':
-          result /= currentNumber;
+          result += ' / ';
           operacion = '/';
           break;
         case '=':
-          switch (operacion) {
-            case '+':
-              result += currentNumber;
-              break;
-            case '-':
-              result -= currentNumber;
-              break;
-            case '*':
-              result *= currentNumber;
-              break;
-            case '/':
-              result /= currentNumber;
-              break;
-          }
+          result = eval(result);
           $('#visor').val(result);
           break;
         case 'C':
-          result = 0;
-          currentNumber = 0;
-          $('#visor').val('');
+          result = '';
+          $('#visor').val(result);
           break;
+        default:
+          return result;
       }
     }
   });
 });
-  
+
