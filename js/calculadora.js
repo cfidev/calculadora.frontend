0dfce28433cedfe12c47145bacb5f90a118c792d
aleecmp <ale.cmpb@gmail.com>
2023-04-11 17:09:16 -0400

    fix: add more fix logic

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,43 +1,89 @@
 $(function() {
-  let result = '';
-  let operation = '';
+    let result = '';
+    let numMinusSigns = 0;
+    let numPlusSigns = 0;
+    let finalResult = null;
 
-  $('.btn').click(function() {
-    let buttonVal = $(this).val();
+    $('#visor').val(0);
 
-    if ($.isNumeric(buttonVal)) {
-      result += buttonVal;
-      $('#visor').val(result);
-    } else {
-      switch(buttonVal) {
-        case '+':
-          result += ' + ';
-          operation = '+';
-          break;
-        case '-':
-          result += ' - ';
-          operation = '-';
-          break;
-        case '*':
-          result += ' * ';
-          operation = '*';
-          break;
-        case '/':
-          result += ' / ';
-          operation = '/';
-          break;
-        case '=':
-          result = eval(result);
-          $('#visor').val(result);
-          break;
-        case 'C':
-          result = '';
-          $('#visor').val(result);
-          break;
-        default:
-          return result;
-      }
-    }
-  });
+    $('.btn').click(function() {
+        let buttonVal = $(this).val();
+
+        if ($.isNumeric(buttonVal)) {
+            if (result === '0') {
+                result = buttonVal;
+            } else {
+                result += buttonVal;
+            }
+            $('#visor').val(result);
+            numMinusSigns = 0;
+            numPlusSigns = 0;
+        } else {
+            switch(buttonVal) {
+                case '+':
+                    if (
+                        result === '' ||
+                        result.endsWith('* ') ||
+                        result.endsWith('/ ') ||
+                        numPlusSigns === 1) {
+                        break;
+                    }
+                    result += ' + ';
+                    $('#visor').val(result)
+                    numMinusSigns = 0;
+                    numPlusSigns++;
+                    break;
+                case '-':
+                    if (result === '' ||
+                        result.endsWith('* ') ||
+                        result.endsWith('/ ') ||
+                        numMinusSigns === 2) {
+                        break;
+                    }
+                    result += ' - ';
+                    $('#visor').val(result)
+                    numPlusSigns = 0;
+                    numMinusSigns++;
+                    break;
+                case '*':
+                    if (result === '' ||
+                        result.endsWith('* ') ||
+                        result.endsWith('/ ')) {
+                        break;
+                    }
+                    result += ' * ';
+                    $('#visor').val(result)
+                    numMinusSigns = 0;
+                    numPlusSigns = 0;
+                    break;
+                case '/':
+                    if (result === '' ||
+                        result.endsWith('* ') ||
+                        result.endsWith('/ ')) {
+                        break;
+                    }
+                    result += ' / ';
+                    $('#visor').val(result)
+                    numMinusSigns = 0;
+                    numPlusSigns = 0;
+                    break;
+                case '=':
+                    finalResult = eval(result);
+                    $('#visor').val(finalResult);
+                    result = finalResult.toString();
+                    numMinusSigns = 0;
+                    numPlusSigns = 0;
+                    break;
+                case 'C':
+                    result = '0';
+                    $('#visor').val(result);
+                    numMinusSigns = 0;
+                    numPlusSigns = 0;
+                    break;
+                default:
+                    return result;
+            }
+        }
+    });
 });
 
